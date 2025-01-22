ef8ec89c21f85607e5d1a87d84417abf713dd01c
MuhammadSarimAsad <sarimofficial3@gmail.com>
2025-01-22 11:48:21 +0500

    removed other template 1 template remaining only

--- code/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ["cdn.sanity.io"], // Correct domain with a dot (.)
-    },
-  };
-  
-  export default nextConfig;
-  
